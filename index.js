c91c76539b1bb57fa6cbfb02952009339ab42f17
i6nake9 <fleshartemka@gmail.com>
2022-07-27 16:20:25 +0300

    v1.01 Add Express.js

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-const fs = require('fs');
+const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
-const http = require('http');
 dotenv.config();
 
-const HOSTNAME = process.env.HOSTNAME;
 const PORT = process.env.PORT;
-
-const server = http.createServer((req, res) => {
-	if (req.url === '/') {
-		res.setHeader('Content-Type', 'text/html');
-		res.write(
-			'<html> <head>Server Response</head><body><h1>Hello!</h1></body></html>'
-		);
-		res.write(
-			'<form action="/create-user" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form>'
-		);
-		return res.end();
-	}
-
-	if (req.url === '/users') {
-		res.write(
-			'<html> <head>Server Response</head><body><ul><li>Users 1</li><li>Users 2</li><li>Users 3</li></ul></body></html>'
-		);
-		return res.end();
-	}
-	if (req.url === '/create-user' && req.method === 'POST') {
-		const bodyChunk = [];
-		req.on('data', (chunk) => {
-			console.log(chunk);
-			bodyChunk.push(chunk);
-		});
-		return req.on('end', () => {
-			const parsedBody = Buffer.concat(bodyChunk).toString();
-			const message = parsedBody.split('=')[1];
-			res.write(`<h1>${message}</h1>`);
-			console.log(message);
-		});
-	}
-	if (req.url === '/create-user' && req.method === 'GET') {
-		res.setHeader('Content-Type', 'text/html');
-		res.write(
-			'<form action="/" method="POST"><button type="submit">Redirect</button></form>'
-		);
-		return res.end();
-	}
+// Test GET from insomnia
+app.get('/', (req, res) => {
+	res.send('Hello world from Node.JS ');
 });
 
-server.listen(PORT, HOSTNAME, () => {
-	console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
+app.listen(PORT, () => {
+	console.log(`Server started on https://localhost:${PORT}`);
 });
